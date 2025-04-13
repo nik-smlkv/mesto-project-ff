a6a42c1f66d445f632cbdea7b77af242024128c6
Никита Смольков <nikitasmolkov@MacBook-Air-Nikita.local>
2025-04-13 15:39:11 +0700

    доработка исправлений

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,25 @@
 import initialCards from "./cards.js";
+const placesList = document.querySelector(".places__list");
 
 const createCard = (data, deleteCard) => {
-
   const cardTemplate = document
     .querySelector("#card-template")
     .content.cloneNode(true);
   const cardElement = cardTemplate.querySelector(".card");
-
+  const cardElementImage = cardElement.querySelector(".card__image");
   cardElement.querySelector(".card__title").textContent = data.name;
-  cardElement.querySelector(".card__image").src = data.link;
-  cardElement.querySelector(".card__image").alt = data.name;
-  cardElement.querySelector(".card__delete-button").addEventListener("click", deleteCard);
+  cardElementImage.src = data.link;
+  cardElementImage.alt = data.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCard);
 
   return cardElement;
 };
 
 const deleteCard = (event) => {
-    let currentCard = event.target.parentElement;
-    currentCard.remove();
+  event.target.parentElement.remove();
 };
-
-const placesList = document.querySelector(".places__list");
 
 initialCards.forEach((cardData) => {
   const cardElement = createCard(cardData, deleteCard);
